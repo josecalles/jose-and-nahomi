127c142f2395c5e0ca39b476f2e9c486fa9ae3d4
Jose Calles <jose@nullstudio.co>
2025-11-27 15:56:30 -0800

    make some updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    devToolbar: {
+        enabled: false
+    },
     site: "https://joseandnahomi.com",
     vite: {
-        plugins: [tailwindcss()]
+        plugins: [tailwindcss()],
+        server: {
+            allowedHosts: ['macbook-pro.tail9203de.ts.net'],
+        },
     }
 });
